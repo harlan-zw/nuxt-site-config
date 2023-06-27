d55117e001273bac622150e6b12b033d222885f0
Harlan Wilton <harlan@harlanzw.com>
2023-06-27 10:35:44 +1000

    fix(build): lazy init site config

--- src/build/init.ts
+++ src/build/init.ts
@@ -1,10 +1,10 @@
 import { tryUseNuxt } from '@nuxt/kit'
 import { readPackageJSON } from 'pkg-types'
-import type { SiteConfig, SiteConfigInput } from '../type'
+import type { SiteConfig, SiteConfigContainer, SiteConfigInput } from '../type'
 import { createSiteConfigContainer } from '../runtime/siteConfig'
 import { envSiteConfig } from '../runtime/siteConfig/env'
 
-export const siteConfigContainer = createSiteConfigContainer()
+let siteConfigContainer: SiteConfigContainer
 
 async function getPkgJsonContextConfig(rootDir: string) {
   const pkgJson = await readPackageJSON(undefined, { startingFrom: rootDir })
@@ -22,21 +22,24 @@ export async function initSiteConfig(siteConfig: SiteConfigInput): Promise<SiteC
   const nuxt = tryUseNuxt()
   const rootDir = nuxt?.options.rootDir || process.cwd()
 
-  const isNodeEnv = !!process.env.NODE_ENV
-  // the root dir is maybe the name of the site
-  siteConfigContainer.push({
-    name: rootDir.split('/').pop(),
-    index: isNodeEnv ? process.env.NODE_ENV === 'production' : !process.dev,
-  })
-  siteConfigContainer.push(await getPkgJsonContextConfig(rootDir))
+  // only when called the first time
+  if (!siteConfigContainer) {
+    siteConfigContainer = createSiteConfigContainer()
+    const isNodeEnv = !!process.env.NODE_ENV
+    // the root dir is maybe the name of the site
+    siteConfigContainer.push({
+      name: rootDir.split('/').pop(),
+      index: isNodeEnv ? process.env.NODE_ENV === 'production' : !process.dev,
+    })
+    siteConfigContainer.push(await getPkgJsonContextConfig(rootDir))
 
-  // add the env vars lowest priority
-  siteConfigContainer.push(envSiteConfig)
-  // not actually needed
-  if (nuxt)
-    siteConfigContainer.push(nuxt?.options.runtimeConfig.public.site)
+    // add the env vars lowest priority
+    siteConfigContainer.push(envSiteConfig)
+    // not actually needed
+    if (nuxt)
+      siteConfigContainer.push(nuxt?.options.runtimeConfig.public.site)
+  }
 
   siteConfigContainer.push(siteConfig)
-
   return siteConfigContainer.get()
 }
