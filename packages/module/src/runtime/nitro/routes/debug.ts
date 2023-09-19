e8e8d56361c777472bb9e7c46074ebf30934169b
Harlan Wilton <harlan@harlanzw.com>
2023-09-19 20:34:14 +0200

    fix: expose version in debug

--- packages/module/src/runtime/nitro/routes/debug.ts
+++ packages/module/src/runtime/nitro/routes/debug.ts
@@ -1,10 +1,17 @@
-import { defineEventHandler, useNitroOrigin, useSiteConfig } from '#imports'
+import { setHeader } from 'h3'
+import { defineEventHandler, useNitroOrigin, useRuntimeConfig, useSiteConfig } from '#imports'
 
 export default defineEventHandler(async (e) => {
   const siteConfig = useSiteConfig(e)
   const nitroOrigin = useNitroOrigin(e)
+  // use version
+  const { public: publicRuntimeConfig } = useRuntimeConfig()
+
+  // add json headers
+  setHeader(e.response, 'Content-Type', 'application/json')
   return {
     ...siteConfig,
     nitroOrigin,
+    version: publicRuntimeConfig['nuxt-site-config']?.version,
   }
 })
