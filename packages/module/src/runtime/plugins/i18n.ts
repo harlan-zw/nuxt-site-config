f0706f484689f0f7a26b4ae0d96ea7dd3ccc3507
Harlan Wilton <harlan@harlanzw.com>
2023-09-29 17:48:33 +0200

    fix: i18n `nuxtSiteConfig` key support and baseUrl

--- packages/module/src/runtime/plugins/i18n.ts
+++ packages/module/src/runtime/plugins/i18n.ts
@@ -1,15 +1,19 @@
-import type { Ref } from 'vue'
-import { defineNuxtPlugin, ref, updateSiteConfig, useSiteConfig } from '#imports'
+import { computed, defineNuxtPlugin, updateSiteConfig } from '#imports'
 
 export default defineNuxtPlugin({
   name: 'nuxt-site-config:i18n',
   // @ts-expect-error todo fix upstream
   enforce: 1, // needs to come after i18n
   setup(nuxtApp) {
-    const i18n = (nuxtApp.$i18n || { locale: ref(useSiteConfig().defaultLocale) }) as { locale: Ref<string> }
+    const i18n = nuxtApp.$i18n
+    if (!i18n)
+      return
     updateSiteConfig({
       _context: '@nuxtjs/i18n',
+      url: i18n.baseUrl.value,
       currentLocale: i18n.locale,
+      description: computed(() => i18n.t('nuxtSiteConfig.description')),
+      name: computed(() => i18n.t('nuxtSiteConfig.name')),
     })
   },
 })
