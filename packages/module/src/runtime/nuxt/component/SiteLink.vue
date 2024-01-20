4e97474852939253f6df51e896cf5e5e6c71b804
Harlan Wilton <harlan@harlanzw.com>
2024-01-20 20:43:08 +0800

    fix(SiteLink): properly extend NuxtLinks props
    Fixes #17

--- packages/module/src/runtime/nuxt/component/SiteLink.vue
+++ packages/module/src/runtime/nuxt/component/SiteLink.vue
@@ -1,22 +1,28 @@
 <script lang="ts" setup>
-import { toRefs } from 'vue'
+import { computed, toRefs } from 'vue'
+import type { NuxtLinkProps } from 'nuxt/app'
 import type { CreateSitePathResolverOptions } from '../../types'
-import { createSitePathResolver } from '#imports'
+import { createSitePathResolver, resolveComponent } from '#imports'
 
-const props = defineProps<{
-  to: string
-} & CreateSitePathResolverOptions>()
+const props = defineProps<CreateSitePathResolverOptions & NuxtLinkProps>()
 
 // make props refs
 const propRefs = toRefs(props)
 
 const linkResolver = createSitePathResolver(propRefs as any as CreateSitePathResolverOptions)
 
-const to = linkResolver(propRefs.to)
+const NuxtLink = resolveComponent('NuxtLink')
+
+const to = computed(() => {
+  const _to = props.to as string | undefined
+  if (!_to)
+    return undefined
+  return linkResolver(_to)
+})
 </script>
 
 <template>
-  <NuxtLink :to="to">
+  <NuxtLink v-bind="$props" :to="to">
     <slot />
   </NuxtLink>
 </template>
