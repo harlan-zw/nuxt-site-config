4a0b1a2f6fa5ebb6393bb56132a1fc69da736362
harlan <harlan@harlanzw.com>
2024-11-25 02:07:58 +1100

    fix: `/urls` subpath export

--- packages/module/build.config.ts
+++ packages/module/build.config.ts
@@ -5,6 +5,7 @@ export default defineBuildConfig({
   entries: [
     { input: 'src/kit', name: 'kit' },
     { input: 'src/utils', name: 'utils' },
+    { input: 'src/urls', name: 'urls' },
   ],
   externals: [
     '#imports',
