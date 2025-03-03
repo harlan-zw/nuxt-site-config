997ee20c6c5167a6462a92f6d9af65cdfe40636c
Harlan Wilton <harlan@harlanzw.com>
2025-03-04 00:22:40 +1100

    fix: handle site config usage in middleware (#45)

--- packages/module/src/runtime/server/composables/useSiteConfig.ts
+++ packages/module/src/runtime/server/composables/useSiteConfig.ts
@@ -4,8 +4,12 @@ import type { NuxtSiteConfig } from '../../types'
 import { defu } from 'defu'
 import { useRuntimeConfig } from 'nitropack/runtime'
 import { createSiteConfigStack } from 'site-config-stack'
+import { logger } from '../util'
 
 export function useSiteConfig(e: H3Event, _options?: GetSiteConfigOptions): NuxtSiteConfig {
+  if (import.meta.dev && !e.context._initedSiteConfig) {
+    logger.warn('Site config has not been initialized yet. If you\'re trying to access site config in a server middleware then this not yet supported. See https://github.com/harlan-zw/nuxt-seo/issues/397')
+  }
   e.context.siteConfig = e.context.siteConfig || createSiteConfigStack()
   const options = defu(_options, useRuntimeConfig(e)['nuxt-site-config'], { debug: false })
   return e.context.siteConfig.get(options) as NuxtSiteConfig
