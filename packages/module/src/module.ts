744e94975e1e2ed11bdaf59ba08e8760ad2d1d6e
Thimo Sietsma <thimosietsma@gmail.com>
2025-03-17 11:41:52 +0100

    fix: nullable chain for getting locales (#49)

--- packages/module/src/module.ts
+++ packages/module/src/module.ts
@@ -195,7 +195,7 @@ declare global {
       // @ts-expect-error untyped
       const baseUrl = nuxt.options.i18n?.baseUrl
       // @ts-expect-error untyped
-      const locale = nuxt.options.i18n?.locales.find(l => l.code === nuxt.options.i18n?.defaultLocale)
+      const locale = nuxt.options.i18n?.locales?.find(l => l.code === nuxt.options.i18n?.defaultLocale)
       updateSiteConfig({
         _context: '@nuxtjs/i18n',
         url: typeof baseUrl === 'string' ? baseUrl : undefined,
